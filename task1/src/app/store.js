11ce659eaa5f97584fae4a92d6729c679c55b9ce
gtmshine <gtmrohit02@gmail.com>
2023-01-13 22:13:43 +0545

    fix: cleaning unused code

--- task1/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import colors from "../features/colors/colorsSlice";
-
-export const store = configureStore({
-  reducer: {
- 
-    colors,
-  },
-});
